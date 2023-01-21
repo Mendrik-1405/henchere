5ac3995b1ef88cafc65b06ab607ee6c441f5390c
mendrika1401 <mendrika1401@gmail.com>
2023-01-20 17:37:02 -0800

    Update EnchereController.java

--- src/main/java/extract/enchere/EnchereController.java
+++ src/main/java/extract/enchere/EnchereController.java
@@ -3,6 +3,7 @@ package extract.enchere;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +18,7 @@ import lombok.EqualsAndHashCode;
 
 @Data
 @RestController
+@CrossOrigin
 @RequestMapping("/enchere")
 @EqualsAndHashCode(callSuper = false)
 public class EnchereController extends CrudController<Enchere, Integer, EnchereRepos, EnchereService> {
